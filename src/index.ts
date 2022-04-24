fbf963d014e5f3e1f5b8998f4a649081731ea45b
pact0 <kuba.kowalski21331@gmail.com>
2022-04-24 19:08:32 +0200

    expose to package

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,1 @@
-import { exampleItems } from "@utils/testData";
-import { generateItem } from "@utils/generateItem";
-
-import fs from "fs";
-const path = require("path");
-
-const data = fs.readFileSync(path.resolve(__dirname, "testItem.txt"), 'utf8')
-
-console.log(generateItem(data))
-
-// list of example items
-// exampleItems.map((x)=>{
-//   console.log(generateItem(x.text))
-// })
+export { generateItem } from "@utils/generateItem";
