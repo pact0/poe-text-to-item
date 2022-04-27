25dde7404fb6a872be7eb0ce137edb5a437325b3
pact0 <kuba.kowalski21331@gmail.com>
2022-04-27 23:17:40 +0200

    changed icon

--- src/utils/getItemIcon.ts
+++ src/utils/getItemIcon.ts
@@ -1,11 +1,14 @@
 import rareBases from "@bases/rareBases.json"
 import uniqueBases from "@bases/uniqueBases.json"
 
-export function getItemIcon(itemBase){
+export function getItemIcon(itemBase:any){
   if(itemBase.rarity !== "Unique"){
+
     if(itemBase.base.includes("Map")) return ""
-    return "http://web.poecdn.com/image/Art/2DItems/" + rareBases[itemBase.base].imgurl
+
+    return "http://web.poecdn.com/image/Art/2DItems/" + rareBases[itemBase.base as keyof typeof rareBases].imgurl
   } else{
-    return "http://web.poecdn.com/image/Art/2DItems/" + uniqueBases[itemBase.name].icon + ".png"
+    const base =uniqueBases[itemBase.name as keyof typeof uniqueBases] as any
+    return "http://web.poecdn.com/image/Art/2DItems/" + base.icon + ".png"
   }
 }
